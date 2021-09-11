da8f7186ae377f61fd2bcb6357423eb05fc294a0
hasibul1610 <bcse.hasibul@gmail.com>
2021-09-11 09:19:43 +0600

    first commit

---
+++ fundamentals.js
@@ -0,0 +1,15 @@
+let a = 6;
+a=8;
+// console.log(a);
+
+const name = "Md. Hasibul Islam Shanto";
+// console.log(name);
+
+const num1 = 8;
+const num2 = 'nine';
+
+if(num1 === 8 && num2 === "eight"){
+    // console.log("The statement is correct");
+}else{
+    // console.log("Statement is not correct");
+}
